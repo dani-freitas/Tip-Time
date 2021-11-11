9a53c4b8a02464f16528717ace8f85c70ac6ba85
Dani_Freitas <85803246+dani-freitas@users.noreply.github.com>
2021-11-11 15:58:28 -0300

    first commit

---
+++ app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.tiptime
+
+import android.os.Binder
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.tiptime.databinding.ActivityMainBinding
+import java.util.zip.Inflater
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(R.layout.activity_menu)
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ app/src/main/java/com/example/tiptime/ui/calculate/CalculateViewModel.kt
@@ -0,0 +1,13 @@
+package com.example.tiptime.ui.calculate
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class CalculateViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is dashboard Fragment"
+    }
+    val text: LiveData<String> = _text
+}
